5319681dfeb4917268f291613b2904a35e65bd76
nattakit <search35453@gmail.com>
2022-01-26 22:26:28 +0700

    added map components and updated mapscreen

---
+++ components/NavigateCard.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native';
+import React from 'react';
+
+const NavigateCard = () => {
+  return (
+    <View>
+      <Text>YO</Text>
+    </View>
+  );
+};
+
+export default NavigateCard;
